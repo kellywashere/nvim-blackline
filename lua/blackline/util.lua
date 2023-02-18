e868f9457d51891f17757fc19a3ea66025408c9c
Kelly was Here <kellywashere@yahoo.com>
2023-02-18 11:33:03 +0100

    Should also work with guitermcolors=false

--- lua/blackline/util.lua
+++ lua/blackline/util.lua
@@ -1,22 +1,24 @@
 local M = {}
 
 function M.set_cursorline(new_cursorline)
-	-- sets cursorline color
-	local stridx1 = string.find(new_cursorline, 'ctermbg')
-	local stridx2 = string.find(new_cursorline, 'guibg')
-	-- check if string is already formatted to go into vim command
-	if (stridx1 or stridx2) then
+	-- new_cursorline can be either a fully formatted string like 'ctermbg=Black guibg=#ff0000'
+	-- or it can be a color name like 'Black'
+	-- or it can be a color nr like '#ff0000'
+
+	if (string.find(new_cursorline, 'cterm') or string.find(new_cursorline, 'gui')) then
 		vim.cmd('highlight CursorLine ' .. new_cursorline)
-	else
+	elseif (vim.o.termguicolors) then
 		vim.cmd('highlight CursorLine guibg=' .. new_cursorline)
+	else
+		vim.cmd('highlight CursorLine ctermbg=' .. new_cursorline)
 	end
 end
 
 function M.get_cursorline()
 	-- returns current content of CursorLine highlight
 	local cursorline = vim.api.nvim_exec('highlight CursorLine', true)
-	local stridx1 = string.find(cursorline, 'ctermbg=')
-	local stridx2 = string.find(cursorline, 'guibg=')
+	local stridx1 = string.find(cursorline, 'cterm')
+	local stridx2 = string.find(cursorline, 'gui')
 	local stridx = stridx1
 	if (not stridx or (stridx2 and stridx2 < stridx)) then
 		stridx = stridx2
